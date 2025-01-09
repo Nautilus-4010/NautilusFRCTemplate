23a4db61e100e8e39a1354e6112c6098d2262beb
MontserratRuiz4010 <A01667713@tec.mx>
2025-01-09 16:01:16 -0600

    Update Camera.java

--- src/main/java/frc/robot/utils/Camera.java
+++ src/main/java/frc/robot/utils/Camera.java
@@ -1,3 +1,21 @@
+
 public class Camera {
-    
+    public String makehttprequest(String urlApi){
+
+        return ;
+        try {
+
+            URL url = new URL(urlApi);
+            URLConnection connection = url.openConnection();
+
+            Reader r = new InputStreamReader(connection.getInputStream());
+            BufferedReader br = new BufferedReader(r);
+            
+            
+            return br.readLine()
+        } catch (IOException e) {
+           return null;
+            
+        }
+    }
 }
